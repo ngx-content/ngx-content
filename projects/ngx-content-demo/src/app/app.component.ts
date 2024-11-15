28ce599c33b50c891e760f58d7281e5b8a177287
Zygimantas Berziunas <zygimantas@berziunas.com>
2024-11-15 23:01:09 +0200

    feat: add

---
+++ projects/ngx-content-demo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgxContentComponent } from 'ngx-content';
+
+@Component({
+  imports: [NgxContentComponent],
+  selector: 'app-root',
+  standalone: true,
+  styleUrl: './app.component.css',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
